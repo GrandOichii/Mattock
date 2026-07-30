f6dba04d4814f33ffa687617c1655cfab825da22
Igor Yastrebov <i.hawk@yandex.ru>
2026-07-30 22:35:13 +0300

    Core: start implementing target class

--- Mattock.Core/Matches/Scripting/Targets/Target.cs
+++ Mattock.Core/Matches/Scripting/Targets/Target.cs
@@ -1,13 +1,17 @@
+using Mattock.Core.Utility;
 using NLua;
 
 namespace Mattock.Core.Matches.Scripting.Targets;
 
 public class Target
 {
+    public LuaFunction GetFunc { get; }
+    public LuaFunction CheckFunc { get; }
 
     public Target(LuaTable table)
     {
-        
+        GetFunc = LuaCommon.Get<LuaFunction>(table, "Get");
+        CheckFunc = LuaCommon.Get<LuaFunction>(table, "Check");
     }
 
     
--- core/target.lua
+++ core/target.lua
@@ -24,7 +24,7 @@ function Target:Player(playersSelect, targetAmount)
             -- TODO
             return {}
         end,
-        EnoughTargets = function ()
+        Check = function ()
             return false
         end
     }
