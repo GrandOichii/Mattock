334b44abf4ec29ac5bfb979a97eb69ae6179ed5d
Igor Yastrebov <i.hawk@yandex.ru>
2026-08-06 21:26:53 +0300

    Core: implement RecorderPlayerControllerWrapper

--- Mattock.Core/Matches/Players/Controllers/RecorderPlayerControllerWrapper.cs
+++ Mattock.Core/Matches/Players/Controllers/RecorderPlayerControllerWrapper.cs
@@ -3,50 +3,73 @@ using Mattock.Core.Matches.Players.Actions;
 using Mattock.Core.Matches.Players.Cards;
 using Mattock.Core.Matches.Players.Controllers.ManaPaymentChoices;
 using Mattock.Core.Matches.Players.Costs;
+using Mattock.Core.Matches.Zones;
 
 namespace Mattock.Core.Matches.Players.Controllers;
 
-// public class PlayerResponsesRecord
-// {
-//     public List<string>
-// }
+public class PlayerResponsesRecord
+{
+    public List<string?> CardChoices { get; } = [];
+    public List<string> CommandChoices { get; } = [];
+    public List<string?> CostCollectionChoices { get; } = [];
+    public List<string> ManaPaymentChoices { get; } = [];
+
+    public List<string[]> PermanentsChoices { get; } = [];
+    public List<int[]> PlayersChoices { get; } = [];
+    public List<string?> StringChoices { get; } = [];
+}
 
 public class RecorderPlayerControllerWrapper(IPlayerController controller)
     : PlayerControllerWrapper(controller)
 {
+    public PlayerResponsesRecord Record { get; } = new();
 
     public override Task HandleCardChoice(Card? choice, Player player, Card[] options, string hint)
     {
-        throw new NotImplementedException();
+        Record.CardChoices.Add(choice?.Id);
+        return Task.CompletedTask;
     }
+    
 
     public override Task HandleCommandChoice(ICommand choice, Player player, ICommand[] choices)
     {
-        throw new NotImplementedException();
+        Record.CommandChoices.Add(choice.ToCommandString());
+        return Task.CompletedTask;
     }
+    
 
     public override Task HandleCostCollectionChoice(CostCollection? choice, Player player, CostCollection[] options, string hint)
     {
-        throw new NotImplementedException();
+        Record.CostCollectionChoices.Add(choice?.Text); // TODO kinda sus, are all texts unique?
+        return Task.CompletedTask;
     }
+    
 
     public override Task HandleManaPaymentChoice(IManaPaymentChoice choice, Player player, IManaPaymentChoice[] options, string hint)
     {
-        throw new NotImplementedException();
+        Record.ManaPaymentChoices.Add(choice.ToDisplayString());
+        return Task.CompletedTask;
     }
+    
 
     public override Task HandlePermanentsChoice(Permanent[] choices, Player player, Permanent[] options, int min, int max, string hint)
     {
-        throw new NotImplementedException();
+        Record.PermanentsChoices.Add([.. choices.Select(p => p.Pid)]);
+        return Task.CompletedTask;
     }
+    
 
     public override Task HandlePlayersChoice(Player[] choices, Player player, Player[] options, int min, int max, string hint)
     {
-        throw new NotImplementedException();
+        Record.PlayersChoices.Add([.. choices.Select(p => p.Idx)]);
+        return Task.CompletedTask;
     }
+    
 
     public override Task HandleStringChoice(string? choice, Player player, string[] options, string hint)
     {
-        throw new NotImplementedException();
+        Record.StringChoices.Add(choice);
+        return Task.CompletedTask;
     }
+    
 }
